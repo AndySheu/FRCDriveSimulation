9dbec8c946a8f9d3ca155a08ea9793be4031d351
Staff <Staff>
2016-03-22 15:45:06 -0700

    Removed random stuff

--- src/org/usfirst/frc/team972/robot/Robot.java
+++ src/org/usfirst/frc/team972/robot/Robot.java
@@ -4,24 +4,102 @@ package org.usfirst.frc.team972.robot;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 public class Robot extends IterativeRobot {
 
-	RobotDrive botDrive = new RobotDrive(0, 1, 2, 3);
+	Victor v0 = new Victor(0);
+	Victor v1 = new Victor(1);
+	RobotDrive botDrive = new RobotDrive(v0, v1);
 
-	Joystick leftJoy = new Joystick(0);
-	Joystick rightJoy = new Joystick(1);
+	Joystick leftJoy;
+	Joystick rightJoy;
+	Joystick joy;
 
-	public void robotInit() {
+	boolean highGear = true, gearshiftPressedLastTime = false;
+	boolean reverseFront = true, reversePressedLastTime = false;
+	boolean squaredInputs = true, squaredPressedLastTime = false;
 
+	boolean joystick = true;
+
+	public void teleopInit() {
+		if (joystick) {
+			leftJoy = new Joystick (0);
+			rightJoy = new Joystick (1);
+			joy = new Joystick (2);
+		}
 	}
 
 	public void teleopPeriodic() {
 
-		double leftDriveSpeed = leftJoy.getY();
-		double rightDriveSpeed = rightJoy.getY();
+		double leftDriveSpeed;
+		double rightDriveSpeed;
+		boolean gearshiftPressed;
+		boolean reversePressed;
+		boolean squaredPressed;
 
+		if (joystick) {
+			leftDriveSpeed = leftJoy.getY();
+			rightDriveSpeed = rightJoy.getY();
+			gearshiftPressed = rightJoy.getRawButton(1);
+			reversePressed = leftJoy.getRawButton(1);
+			squaredPressed = leftJoy.getRawButton(1);
+		} else {
+			// Doesn't actually work
+			leftDriveSpeed = leftJoy.getY();
+			rightDriveSpeed = rightJoy.getY();
+			gearshiftPressed = rightJoy.getRawButton(1);
+			reversePressed = leftJoy.getRawButton(1);
+			squaredPressed = leftJoy.getRawButton(1);
+		}
+
+		// Check gearshift
+		if (gearshiftPressed && !gearshiftPressedLastTime) {
+			highGear = !highGear;
+		}
+		gearshiftPressedLastTime = gearshiftPressed;
+
+		// Check reverse front
+		if (reversePressed && !reversePressedLastTime) {
+			reverseFront = !reverseFront;
+		}
+		reversePressedLastTime = reversePressed;
+
+		// Check squared inputs
+		if (squaredPressed && !squaredPressedLastTime) {
+			squaredInputs = !squaredInputs;
+		}
+		squaredPressedLastTime = squaredPressed;
+
+		// Apply Squared inputs
+		if (squaredInputs) {
+			if (leftDriveSpeed < 0) {
+				leftDriveSpeed = -Math.pow(leftDriveSpeed, 2);
+			} else if (leftDriveSpeed > 0) {
+				leftDriveSpeed = Math.pow(leftDriveSpeed, 2);
+			}
+			if (rightDriveSpeed < 0) {
+				rightDriveSpeed = -Math.pow(rightDriveSpeed, 2);
+			} else if (rightDriveSpeed > 0) {
+				rightDriveSpeed = Math.pow(rightDriveSpeed, 2);
+			}
+		}
+
+		// Apply gearshift
+		if (!highGear) {
+			leftDriveSpeed /= 2;
+			rightDriveSpeed /= 2;
+		}
+
+		// Apply reverse front
+		if (reverseFront) {
+			leftDriveSpeed *= -1;
+			rightDriveSpeed *= -1;
+		}
+
+		System.out.println("LS: " + leftDriveSpeed);
+		System.out.println("RS: " + rightDriveSpeed);
 		botDrive.tankDrive(leftDriveSpeed, rightDriveSpeed);
-
 	}
 }
